d34d828768d899d9c8b4d7fa95992ed5f5843ead
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2023-03-28 22:15:42 -0300

    update main

---
+++ ajudando_o_doutor.py
@@ -0,0 +1,7 @@
+email_aluno = input("Insira o e-mail do aluno: ")
+nota_aluno = input("Insira a sua nota do semestre: ")
+nota_aluno = float(nota_aluno)
+
+if nota_aluno > 8.5:
+    print("ENVIANDO CONVITE")
+    
