2d6d6be0cafe6b116ffa895a15b3bb0cae162261
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2023-03-28 22:18:59 -0300

    update ajudando_o_doutor

--- ajudando_o_doutor.py
+++ ajudando_o_doutor.py
@@ -1,7 +1,11 @@
+#solicitando as informações do aluno
 email_aluno = input("Insira o e-mail do aluno: ")
 nota_aluno = input("Insira a sua nota do semestre: ")
+#convertendo a nota para float
 nota_aluno = float(nota_aluno)
 
+#condição se a nota for maior que 8.5
 if nota_aluno > 8.5:
-    print("ENVIANDO CONVITE")
-    
+    print("ENVIANDO CONVITE PARA {}".format(email_aluno))
+else:
+    print("SUA NOTA ATINGIU A META")
